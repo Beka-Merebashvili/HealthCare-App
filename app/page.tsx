2e0856fd8eebad02d0acf1a536373989bf219d62
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-09-28 15:30:22 +0400

    install shadcn dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-    <div className="text-white text-3xl">HELLO</div>
+    
+    <Button >CLICK HERE</Button>
   );
 }
