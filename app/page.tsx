bc6e0cdd175e7737989b928107da49879cfd5e90
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-10-02 17:48:19 +0400

    add assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import { Button } from "@/components/ui/button";
+ import Image from "next/image";
 
 
 export default function Home() {
   return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          {/* <Image 
+          src={}
+          alt=""
+          /> */}
+        </div>
+      </section>
+    </div>
     
-    <Button >CLICK HERE</Button>
   );
 }
