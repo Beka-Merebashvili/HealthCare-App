d59a5c803daf909b269c57da2ca22d046d484213
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-10-05 11:45:29 +0400

    add validatoons for patient form

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,55 +1,65 @@
-"use client"
- 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+"use client";
 
-import { Button } from "@/components/ui/button"
-import { Form } from "@/components/ui/form"
-import CustomFormField, { FormFieldType } from "../CustomFormField"
-import { useState } from "react"
-import SubmitButton from "../SubmitButton"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
- 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
- 
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import CustomFormField, { FormFieldType } from "../CustomFormField";
+import { useState } from "react";
+import SubmitButton from "../SubmitButton";
+import { UserFormValidation } from "@/lib/validation";
+
+
+
 const PatientForm = () => {
   const [isLoading, setIsLoading] = useState(false);
-  
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
-  })
- 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-   
-    console.log(values)
-  }
+  });
+
+  const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
+    setIsLoading(true);
+
+    try {
+      const user = {
+        name: values.name,
+        email: values.email,
+        phone: values.phone,
+      };
+     
+    } catch (error) {
+      console.log(error);
+    }
+
+    setIsLoading(false);
+  };
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-      <section className="mb-12 space-y-4">
+        <section className="mb-12 space-y-4">
           <h1 className="header">Hi there 👋</h1>
           <p className="text-dark-700">Get started with appointments.</p>
         </section>
 
-        <CustomFormField 
-        fieldType={FormFieldType.INPUT}
-        control={form.control}
-        name="name"
-        label="Full name"
-        placeholder="John Doe"
-        iconSrc="/assets/icons/user.svg"
-        iconAlt="user"
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="name"
+          label="Full name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
         />
 
-<CustomFormField
+        <CustomFormField
           fieldType={FormFieldType.INPUT}
           control={form.control}
           name="email"
@@ -67,13 +77,10 @@ const PatientForm = () => {
           placeholder="(555) 123-4567"
         />
 
-       
-<SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
-  )
-    
-  
-}
+  );
+};
 
-export default PatientForm
+export default PatientForm;
