6f8cd6ccba5cbba5a5e594468d977babcf177c8d
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-10-29 11:23:06 +0400

    update admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,12 @@
 import StatCard from "@/components/StatCard";
 import Image from "next/image";
 import Link from "next/link";
+import { columns } from "@/components/table/columns";
+import { DataTable } from "../../components/table/DataTable";
+import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
 
-const Admin = () => {
+const AdminPage = async () => {
+  const appointments = await getRecentAppointmentList();
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
       <header className='admin-header'>
@@ -28,29 +32,29 @@ const Admin = () => {
         </section>
 
         <section className="admin-stat">
-          <StatCard
+        <StatCard
             type="appointments"
-            count={5}
+            count={appointments.scheduledCount}
             label="Scheduled appointments"
             icon={"/assets/icons/appointments.svg"}
           />
           <StatCard
             type="pending"
-            count={1}
+            count={appointments.pendingCount}
             label="Pending appointments"
             icon={"/assets/icons/pending.svg"}
           />
           <StatCard
             type="cancelled"
-            count={5}
+            count={appointments.cancelledCount}
             label="Cancelled appointments"
             icon={"/assets/icons/cancelled.svg"}
           />
         </section>
-        
+        <DataTable columns={columns} data={appointments.documents} />
       </main>
     </div>
   )
 }
 
-export default Admin
+export default AdminPage;
